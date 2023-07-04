e64194c5aaba34f9f49b3d3bfdbdadf8762bc9e6
Khairul Anwar <khairul87@gmail.com>
2023-07-05 01:12:33 +0800

    Add assignment js

---
+++ JSandTS/OCB-Assignment-230704.js
@@ -0,0 +1,18 @@
+// Assignment Tuan Razman
+
+for (let x = 1; x <= 100; x++) {
+    if (x % 7 == 0) {
+        console.log("Hebat");
+    } else {
+        console.log(x);
+    }
+}
+
+// Assignment Guru Besar #1
+
+var haiwanZoo = ['Panda', 'Harimau', 'Ular', 'Oyen', 'Gajah', 'Unta', 'Zirafah', 'Penguin', 'Burung', 'Bunny'];
+
+for (let i = 0; i<=haiwanZoo.length-1; i++) {
+    console.log(haiwanZoo[i]);
+}
+
