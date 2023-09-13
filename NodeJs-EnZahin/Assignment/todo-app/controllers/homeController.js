4302400635d48299ef2fbaa0d6366e0892a228ba
Khairul Anwar <khairul87@gmail.com>
2023-09-13 09:06:57 +0800

    230912

--- NodeJs-EnZahin/Assignment/todo-app/controllers/homeController.js
+++
@@ -1,8 +0,0 @@
-const homeController = (req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    res.send(
-        "<html><head><title>NodeJs Assignment</title></head><body><h1>Home Controller</h1></body></html>"
-    );
-};
-
-module.exports = homeController;
--- NodeJs-EnZahin/Assignment/todo-app/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-const userController = (req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    res.send(
-        `<html><head><title>NodeJs Assignment</title></head><body><h1>User Controller</h1><h2>Hello ${username}!</h2></body></html>`
-    );
-};
-
-module.exports = userController;
